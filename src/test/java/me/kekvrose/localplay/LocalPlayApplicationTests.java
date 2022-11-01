a5fd01a47eb56147f314344414303bba8e97e502
Christopher Rose <christophervrose@gmail.com>
2022-11-01 15:28:24 +0530

    [trim] remove context for springboot app test

--- src/test/java/me/kekvrose/localplay/LocalPlayApplicationTests.java
+++ src/test/java/me/kekvrose/localplay/LocalPlayApplicationTests.java
@@ -3,10 +3,10 @@ package me.kekvrose.localplay;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// @SpringBootTest
 class LocalPlayApplicationTests {
 
-	@Test
+	// @Test
 	void contextLoads() {
 	}
 
